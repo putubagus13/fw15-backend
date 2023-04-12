769f18b3823971311b9844ed740271ca49918c16
putubagus13 <putu031216@gmail.com>
2023-04-12 09:34:23 +0700

    cek request update

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/auth.controller")
 
 authRouter.post("/login",authController.login)
 authRouter.post("/register",authController.register)
-authRouter.patch("/update",authController.update)
+authRouter.patch("/updateUserNow",authController.updateUserNow)
 
 module.exports = authRouter
