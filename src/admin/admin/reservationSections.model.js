d44619d42b21ee9f1ce4d4ec419d1ee012c329cb
putubagus13 <putu031216@gmail.com>
2023-04-23 21:13:52 +0700

    commit (input model into model/admin folder)

---
+++ src/admin/admin/reservationSections.model.js
@@ -0,0 +1,61 @@
+const db = require("../../helpers/db.helper")
+
+const table = "reservationSections"
+
+exports.findAll = async function(page, limit, search, sort, sortBy){
+    page = parseInt(page) || 1
+    limit = parseInt(limit) || 5
+    search = search || ""
+    sort = sort || "id"
+    sortBy = sortBy || "ASC"
+    const offset = (page -1)* limit
+    const query= `
+    SELECT * FROM "${table}" WHERE "name" LIKE $3 ORDER BY ${sort} ${sortBy} LIMIT $1 OFFSET $2`
+
+    const values = [limit, offset, `%${search}%`]
+    const {rows} = await db.query(query,values)
+    return rows
+}
+
+exports.insert = async function(data){
+    const query = `
+    INSERT INTO "${table}" ("name","price")
+    VALUES ($1, $2) RETURNING *
+    `
+    const values = [data.name, data.price]
+    const {rows} = await db.query(query, values)
+    return rows[0]
+} 
+
+exports.update = async function(id, data){
+    const query = `
+    UPDATE "${table}" 
+    SET 
+    "name"= COALESCE(NULLIF($2,''), "name"),
+    "price"= COALESCE(NULLIF($3,''), "price")
+     WHERE "id"=$1
+    RETURNING *
+    `
+    const values = [id, data.name, data.price]
+    const {rows} = await db.query(query, values)
+    return rows[0]
+} 
+
+exports.destroy = async function(id){
+    const query = `
+    DELETE FROM "${table}" WHERE "id"=$1 RETURNING *
+    `
+    const values = [id]
+    const {rows} = await db.query(query, values)
+    return rows[0]
+} 
+
+exports.findOne = async function(id){
+    const query =`
+    SELECT * FROM "${table}" WHERE id=$1`
+
+    const values = [id]
+    const {rows} = await db.query(query, values)
+    return rows[0]
+}
+
