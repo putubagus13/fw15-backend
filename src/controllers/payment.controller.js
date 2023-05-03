5ffdfa45f5d2dc75ce854eb9116ef3f3213db681
putubagus13 <putu031216@gmail.com>
2023-05-03 08:18:09 +0700

    update /payment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,40 +1,28 @@
-const paymentMethodModel = require("../model/admin/paymentMethod.model")
+const reservationModel = require("../model/admin/reservations.model")
 const errorHandler = require("../helpers/errorHandler")
-
-exports.getAllPayment = async (request,response)=>{
-    try {
-        const data = await paymentMethodModel.findAll(
-            request.query.page, 
-            request.query.limit, 
-            request.query.search, 
-            request.query.sort, 
-            request.query.sortBy)
-
-        return response.json({
-            success: true,
-            massage: "List of all payment method",
-            results: data
-        })
-    } catch (error) {
-        return errorHandler(response, error)
-    }
-}
+const paymentMethodModel = require("../model/admin/paymentMethod.model")
 
 exports.createPayment = async (request, response)=>{
-    try{
+    try {
+        const paymentMethod = await paymentMethodModel.findOne(request.body.paymentMethodId)
+        if(!paymentMethod){
+            throw Error("Payment method not found")
+        }
         const data = {
             ...request.body
         }
-        const payment = await paymentMethodModel.insert(data)
+    
+        const payment = await reservationModel.update(request.body.reservationId, data)
         if(!payment){
             return Error("update_failed")
         }
+
         return response.json({
             success: true,
-            masssage: "create payment method successfuly",
-            result: payment
+            message: "Payment request Success!",
+            results: payment
         })
-    }catch(error){
-        return errorHandler(response, error)
+    } catch (error) {
+        return errorHandler(response,error)
     }
 }
--- src/routers/payment.router.js
+++ src/routers/payment.router.js
@@ -3,6 +3,5 @@ const paymentMethodController = require("../controllers/payment.controller")
 const validation = require("../middlewares/validator.middlewere")
 
 paymentMethodRouter.post("/", validation("getAllPayment"), paymentMethodController.createPayment)
-paymentMethodRouter.get("/", validation("getAllPayment"), paymentMethodController.getAllPayment)
 
 module.exports = paymentMethodRouter
