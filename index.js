5ffbeb3f9c2e4238f2903d0e2df14072a38a8775
putubagus13 <putu031216@gmail.com>
2023-05-07 22:57:11 +0700

    update new data image

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ require("dotenv").config({
 })
 
 const express = require("express")
-
+const cors = require("cors")
 const app = express()
-
 app.use(express.urlencoded({extended: false}))
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    optionalSuccessStatus: 200
+}))
+
+app.use("/uploads", express.static("uploads"))
 
 app.use("/", require("./src/routers"))
 
