d2b0306c72d2bf768ee39e4f2be2b4a2d2b82394
putubagus13 <putu031216@gmail.com>
2023-05-09 17:32:24 +0700

    update show data

--- src/model/admin/categories.model.js
+++ src/model/admin/categories.model.js
@@ -2,35 +2,17 @@ const db = require("../../helpers/db.helper")
 
 const tabel = "categories"
 
-exports.findAll = async function(page, limit, search, location, sort, sortBy){
+exports.findAll = async function(page, limit, search, sort, sortBy){
     page = parseInt(page) || 1
     limit = parseInt(limit) || 5
     search = search || ""
-    location = location || ""
     sort = sort || "id"
     sortBy = sortBy || "ASC"
     const offset = (page -1)* limit
     const query= `
-    SELECT 
-    "ct"."id",
-    "e"."picture",
-    "e"."title",
-    "e"."desciption",
-    "e"."date",
-    "c"."name" as "location",
-    "ct"."name" as "category",
-    "e"."createdAt",
-    "e"."updatedAt"
-    FROM "categories" "ct"
-    JOIN "eventCategories" "ec" ON "ec"."categoryId" = "ct"."id"
-    JOIN "events" "e" ON "ec"."eventId" = "e"."id"
-    JOIN "cities" "c" ON "c"."id" = "e"."cityId"
-    WHERE "ct"."name" LIKE $1
-    AND "c"."name" LIKE $2
-    ORDER BY ${sort} ${sortBy} 
-    LIMIT $3 OFFSET $4`
+    SELECT * FROM "${tabel}" WHERE "name" LIKE $3 ORDER BY ${sort} ${sortBy} LIMIT $1 OFFSET $2`
 
-    const values = [`%${search}%`, `%${location}%`, limit, offset]
+    const values = [limit, offset, `%${search}%`]
     const {rows} = await db.query(query,values)
     return rows
 }
