f2f4ff9cf21cf57fc9877b3160d93b25f3a57902
putubagus13 <putu031216@gmail.com>
2023-05-15 05:52:33 +0700

    remove validation

--- src/routers/cities.router.js
+++ src/routers/cities.router.js
@@ -1,7 +1,7 @@
 const citiesRouter = require("express").Router()
 const citiesController = require("../controllers/cities.controller")
-const validation = require("../middlewares/validator.middlewere")
+// const validation = require("../middlewares/validator.middlewere")
 
-citiesRouter.get("/",validation("getAllCities"), citiesController.getAllCities)
+citiesRouter.get("/", citiesController.getAllCities)
 
 module.exports = citiesRouter
