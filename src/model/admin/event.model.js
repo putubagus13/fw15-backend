cd2d0a3125af91463ab1f5211b43c23d459cb8c5
putubagus13 <putu031216@gmail.com>
2023-07-05 00:20:31 +0700

    fix: findAll

--- src/model/admin/event.model.js
+++ src/model/admin/event.model.js
@@ -26,9 +26,9 @@ exports.findAll = async function(page, limit, search, category, location, sort, 
     JOIN "cities" "c" ON "c"."id" = "e"."cityId"
     JOIN "eventCategories" "ec" ON "ec"."eventId" = "e"."id"
     JOIN "categories" "ct" ON "ct"."id" = "ec"."categoryId"
-    WHERE "e"."title" LIKE $1
-    AND "ct"."name" LIKE $2
-    AND "c"."name" LIKE $3
+    WHERE "e"."title" ILIKE $1
+    AND "ct"."name" ILIKE $2
+    AND "c"."name" ILIKE $3
     ORDER BY ${sort} ${sortBy} 
     LIMIT $4 OFFSET $5`
 
