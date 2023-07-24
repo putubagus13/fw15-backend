aff783305d01d7c1c0de575ae530a37d5d24a187
putubagus13 <putu031216@gmail.com>
2023-07-24 21:17:01 +0700

    fix: origin

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.urlencoded({extended: false}))
 
 app.use(cors({
-    origin: "https://tixevent.netlify.app/",
+    origin: "http://localhost:8888",
+    // "https://tixevent.netlify.app/",
     optionalSuccessStatus: 200
 }))
 
