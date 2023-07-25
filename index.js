09d39a2ea13eff9b2b769f8ca53d7f5330208df9
Putu Bagus Raditya <127736637+putubagus13@users.noreply.github.com>
2023-07-25 08:47:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.urlencoded({extended: false}))
 
 app.use(cors({
-    origin: "http://localhost:8888",
+    origin: "https://tixevent.netlify.app",
     optionalSuccessStatus: 200
 }))
 
